3c6f4fd6b102dba9df53e658a265535fdcce63d8
okoyet <okoyet@sheridancollege.ca>
2026-07-30 19:22:26 -0400

    Initial Smart Fitness Tracker project setup

---
+++ app/src/main/java/week11/st991708650/smartfitnesstracker/MainActivity.kt
@@ -0,0 +1,31 @@
+package week11.st991708650.smartfitnesstracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+
+            MaterialTheme {
+
+                Surface {
+
+                    Text(
+                        text = "Smart Fitness Tracker"
+                    )
+
+                }
+
+            }
+
+        }
+    }
+}
